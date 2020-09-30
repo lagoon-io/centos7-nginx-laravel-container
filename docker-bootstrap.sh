10609aa0ddc6675d039b4052465e8d4a4be359d1
Kento Takano <kento2github@gmail.com>
2020-10-01 08:08:07 +0900

    Update docker-bootstrap.sh

--- docker-bootstrap.sh
+++ docker-bootstrap.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
-export COMPOSER_HOME="/usr/bin/composer"
 
 if [ -d "/var/www/app/public/app" ] && [ ! -d "/var/www/app/public/vendor" ]; then
   cd /var/www/app/public
   chmod -R 777 ./*
   composer install
+else
+  cd /var/www/app/public
+  composer create-project "laravel/laravel=6.*.*" laravelapp
+  chmod -R 777 ./*
+  # .始まりのファイルもコピーできるようにする
+  shopt -s dotglob
+  cp -r /var/www/app/public/laravelapp/* /var/www/app/public/
+  rm -rf laravelapp
+  cp .env.example .env
+  php artisan key:generate
+  php artisan config:cache
 fi
